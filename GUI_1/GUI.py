ed9263525f66ddc1a79318cdf63bdbbd5df7c770
alekseis1 <aleksei.semikin@gmail.com>
2024-07-09 16:07:24 +0100

    added and updated GUI project

--- GUI_1/GUI.py
+++ GUI_1/GUI.py
@@ -1,6 +1,36 @@
 from tkinter import *
 
-window = Tk() #create a window
-window.geometry("420x420") #resize windows( pixels )
+def submit():
+    username = entry.get()
+    print("hello " + username)
+
+def delete():
+    entry.delete(0,END)
+
+def backspace():
+    entry.delete(len(entry.get())-1, END)
+
+
+window = Tk() #create a windows
+
+
+entry = Entry(window,
+              font = ("Arial", 50))
+entry.pack(side=LEFT)
+entry.insert(0, 'SPONGEBOB')
+
+submit_btn = Button(window,text = 'Submit', command = submit)
+
+submit_btn.pack(side=RIGHT)
+
+delete_btn = Button(window,text = 'Delete', command = delete)
+
+delete_btn.pack(side=RIGHT)
+
+backspace_btn = Button(window,text = '<-', command = backspace)
+
+backspace_btn.pack(side=RIGHT)
+
+
 
 window.mainloop() #places window on computer screen, listens for events
