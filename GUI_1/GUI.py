bb7cb51baad9513b535a48286e4dd738362bc03f
alekseis1 <aleksei.semikin@gmail.com>
2024-07-09 14:35:05 +0100

    moved file to new folder

---
+++ GUI_1/GUI.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+window = Tk() #create a window
+window.geometry("420x420") #resize windows( pixels )
+
+window.mainloop() #places window on computer screen, listens for events
